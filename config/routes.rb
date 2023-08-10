a6a90269938e5d00e98656919a7c304ff19c8ac3
Vincent Tee <vincent.teeym@gmail.com>
2023-08-10 18:43:23 +1000

    Watch List

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
---
+++ db/migrate/20230810032615_add_fields_to_bookmarks.rb
@@ -0,0 +1,5 @@
+class AddFieldsToBookmarks < ActiveRecord::Migration[7.0]
+  def change
+    add_column :bookmarks, :comment, :text
+  end
+end
